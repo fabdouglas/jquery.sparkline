1f1472941e5c52993763b10576e3ab6fb07280f1
Ger Hobbelt <ger@hobbelt.com>
2012-12-06 09:33:38 +0100

    $.browser.xxx crashes in jQuery 1.9.x; using bleeding edge jquery $.support.hasCanvas and $.support.hasVML instead.  Note that the old canvas check has been removed as it was incorrect anyway: see the Modernizr canvas test.

--- src/vcanvas-base.js
+++ src/vcanvas-base.js
@@ -1,12 +1,8 @@
     // Setup a very simple "virtual canvas" to make drawing the few shapes we need easier
     // This is accessible as $(foo).simpledraw()
 
-    if ($.browser.msie && document.namespaces && !document.namespaces.v) {
+    if ($.support.hasVML && document.namespaces && !document.namespaces.v) {
         document.namespaces.add('v', 'urn:schemas-microsoft-com:vml', '#default#VML');
-    }
-
-    if ($.browser.hasCanvas === undefined) {
-        $.browser.hasCanvas = document.createElement('canvas').getContext !== undefined;
     }
 
     VShape = createClass({
