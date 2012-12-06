9db2a3de46cc15e06b7642c2865e9a56e7645ed5
Ger Hobbelt <ger@hobbelt.com>
2012-12-06 09:05:34 +0100

    fix: code was broken for jQuery 1.9.x; now uses $.support (requires bleeding edge jQuery support module though)

--- src/simpledraw.js
+++ src/simpledraw.js
@@ -10,9 +10,9 @@
         if (height === undefined) {
             height = $(this).innerHeight();
         }
-        if ($.browser.hasCanvas) {
+        if ($.support.hasCanvas) {
             target = new VCanvas_canvas(width, height, this, interact);
-        } else if ($.browser.msie) {
+        } else if ($.support.hasVML) {
             target = new VCanvas_vml(width, height, this);
         } else {
             return false;
