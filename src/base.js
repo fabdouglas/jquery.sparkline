91469fe821ff71415018cd9a7ce43d4825f663e2
James Ledoux <jamesrledoux@gmail.com>
2013-01-31 10:48:09 -0500

    Add Bin Labels To Tooltips

--- src/base.js
+++ src/base.js
@@ -274,7 +274,8 @@
                 entries = [],
                 fields, formats, formatlen, fclass, text, i,
                 showFields, showFieldsKey, newFields, fv,
-                formatter, format, fieldlen, j;
+                formatter, format, fieldlen, j, 
+	        label_prefix, label_suffix;
             if (this.currentRegion === undefined) {
                 return '';
             }
@@ -317,12 +318,24 @@
                     format = new SPFormat(format);
                 }
                 fclass = format.fclass || 'jqsfield';
+		
                 for (j = 0; j < fieldlen; j++) {
                     if (!fields[j].isNull || !options.get('tooltipSkipNull')) {
+			label_prefix = '';
+			label_suffix = '';
+			if (options.get('tooltipPrefixBinLabels') 
+			    && (options.get('tooltipPrefixBinLabels').length > fields[j].offset)) {
+			    label_prefix = options.get('tooltipPrefixBinLabels')[fields[j].offset];
+			} 
+			if (options.get('tooltipSuffixBinLabels') 
+			    && (options.get('tooltipSuffixBinLabels').length > fields[j].offset)) {
+			    label_suffix = options.get('tooltipSuffixBinLabels')[fields[j].offset];
+			}
                         $.extend(fields[j], {
-                            prefix: options.get('tooltipPrefix'),
-                            suffix: options.get('tooltipSuffix')
+                            prefix: label_prefix + options.get('tooltipPrefix'),
+                            suffix: options.get('tooltipSuffix') + label_suffix
                         });
+
                         text = format.render(fields[j], options.get('tooltipValueLookups'), options);
                         entries.push('<div class="' + fclass + '">' + text + '</div>');
                     }
